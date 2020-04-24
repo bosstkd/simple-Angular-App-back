3ee40bd62e2960c46f3525f6969372f32be90a18
Mahmoudi Mohammed El Amine <a-ek@hotmail.fr>
2020-04-24 14:57:21 +0200

    upload file spring boot

--- src/main/java/com/myStudies/pjt/controller/produitRestController.java
+++ src/main/java/com/myStudies/pjt/controller/produitRestController.java
@@ -1,8 +1,14 @@
 package com.myStudies.pjt.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +16,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.myStudies.pjt.entities.Produit;
 import com.myStudies.pjt.repository.productRepository;
@@ -55,6 +65,18 @@ public class produitRestController {
 			e.printStackTrace();
 		}
 		return false;
+	}
+	
+	@RequestMapping(value="/upload", method=RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) throws IOException{
+		
+		File convertFile = new File("C:/fileTestSpring/"+file.getOriginalFilename());
+	
+		convertFile.createNewFile();
+		FileOutputStream fout = new FileOutputStream(convertFile);
+		fout.write(file.getBytes());
+		fout.close();
+		return new ResponseEntity<>("File is uploaded successfully", HttpStatus.OK);
 	}
 	
 }
